19d061bdbc5adfb1a29f83d7d9b98f00d9d29f2e
srifimohamed2006-dev <srifi.mohamed2006@gmail.com>
2026-07-30 06:35:58 +0100

    Implement GitHub OAuth callback handler

---
+++ netlify/functions/callback.js
@@ -0,0 +1,61 @@
+const https = require("https");
+
+exports.handler = async (event) => {
+  const code = event.queryStringParameters.code;
+
+  const tokenData = JSON.stringify({
+    client_id: process.env.OAUTH_CLIENT_ID,
+    client_secret: process.env.OAUTH_CLIENT_SECRET,
+    code: code,
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    const req = https.request(
+      {
+        hostname: "github.com",
+        path: "/login/oauth/access_token",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Length": tokenData.length,
+          Accept: "application/json",
+        },
+      },
+      (res) => {
+        let data = "";
+        res.on("data", (chunk) => (data += chunk));
+        res.on("end", () => {
+          try {
+            const parsed = JSON.parse(data);
+            resolve(parsed.access_token);
+          } catch (e) {
+            reject(e);
+          }
+        });
+      }
+    );
+    req.on("error", reject);
+    req.write(tokenData);
+    req.end();
+  });
+
+  const script = `
+    <script>
+      const receiveMessage = (message) => {
+        window.opener.postMessage(
+          'authorization:github:success:${JSON.stringify({ token: accessToken, provider: "github" })}',
+          message.origin
+        );
+        window.removeEventListener("message", receiveMessage, false);
+      };
+      window.addEventListener("message", receiveMessage, false);
+      window.opener.postMessage("authorizing:github", "*");
+    </script>
+  `;
+
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/html" },
+    body: script,
+  };
+};
